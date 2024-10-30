39473a7c6d9270fae35eb88ba93bc541f65dcfcf
BalamiRR <fuatkara.95@gmail.com>
2024-10-30 20:14:34 +0100

    new commit

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -13,22 +13,20 @@ public class AnalyticsCounter {
         Map<String, Integer> symptomCounts = new TreeMap<>();
 
         try (BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"));
-             FileWriter writer = new FileWriter("result.out")) {
+            FileWriter writer = new FileWriter("result.out")) {
 
-            String line = reader.readLine();
+                String line = reader.readLine();
 
-            // Loop to read each line from the file
-            while (line != null) {
-                line = line.trim().toLowerCase();
-                symptomCounts.put(line, symptomCounts.getOrDefault(line, 0) + 1);
-                line = reader.readLine(); // Read next line
-            }
+                // Loop
+                while (line != null) {
+                    line = line.trim().toLowerCase();
+                    symptomCounts.put(line, symptomCounts.getOrDefault(line, 0) + 1);
+                    line = reader.readLine(); // Read next line
+                }
 
-            // Writing the results to output file
-            for (Map.Entry<String, Integer> entry : symptomCounts.entrySet()) {
-                writer.write(entry.getKey() + ": " + entry.getValue() + "\n");
-            }
-
+                for (Map.Entry<String, Integer> entry : symptomCounts.entrySet()) {
+                    writer.write(entry.getKey() + ": " + entry.getValue() + "\n");
+                }
         } catch (IOException e) {
             System.err.println("An error occurred while processing the file: " + e.getMessage());
             e.printStackTrace();
